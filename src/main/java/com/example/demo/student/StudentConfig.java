d5ee13720f1ab29b2a9ef211934c4db69cbd7bcf
Mikey <mikeywearing@gmail.com>
2021-08-13 16:46:44 +0200

    First Commit

---
+++ src/main/java/com/example/demo/student/StudentConfig.java
@@ -0,0 +1,36 @@
+package com.example.demo.student;
+
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import java.time.LocalDate;
+import java.time.Month;
+import java.util.List;
+
+@Configuration
+public class StudentConfig {
+
+    @Bean
+    CommandLineRunner commandLineRunner(StudentRepository repository) {
+        return args -> {
+            Student Mikey = new Student(
+                    "Mikey",
+                    "mikeywearing@gmail.com",
+                    LocalDate.of(1991, Month.DECEMBER, 27),
+                    29
+            );
+
+            Student Ste = new Student(
+                    "Ste",
+                    "ste@gmail.com",
+                    LocalDate.of(1993, Month.OCTOBER, 29),
+                    27
+            );
+
+            repository.saveAll(
+                    List.of(Mikey, Ste)
+            );
+        };
+    }
+}
